d840c2bf2ca03bca3124616599cec8a396922751
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-05-27 19:27:34 +0200

    make metric a choice

--- src/the_segmentation_game/_dock_widget.py
+++ src/the_segmentation_game/_dock_widget.py
@@ -1,7 +1,7 @@
 import warnings
 
 from napari_plugin_engine import napari_hook_implementation
-from qtpy.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QLabel
+from qtpy.QtWidgets import QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QLabel, QLineEdit
 from napari_tools_menu import register_dock_widget
 from magicgui.widgets import create_widget
 from napari.layers import Labels
@@ -31,14 +31,34 @@ class TheSegmentationGameWidget(QWidget):
         segmentation_result_container.layout().addWidget(self.segmentation_result_select.native)
         self.layout().addWidget(segmentation_result_container)
 
+        # widgets for comparison method selection
+        from .metrics import Metrics
+        comparison_method_container = QWidget()
+        comparison_method_container.setLayout(QHBoxLayout())
+        comparison_method_container.layout().addWidget(QLabel("Comparison Method"))
+        self.comparison_method_select = create_widget(
+            widget_type="ComboBox",
+            name="comparison_method",
+            value=Metrics.Jaccard_Index_sparse,
+            options={"choices": [e for e in Metrics]},
+        )
+        comparison_method_container.layout().addWidget(
+            self.comparison_method_select.native
+        )
+        self.layout().addWidget(comparison_method_container)
+
+        # widget for quality visualization
         self.quality_text = QLabel("")
         font = self.quality_text.font()
         font.setPointSize(30)
         self.quality_text.setFont(font)
 
         self.setLayout(QVBoxLayout())
-        self.layout().addWidget(QLabel("Jaccard Index (sparse):"))
         self.layout().addWidget(self.quality_text)
+
+        self.description_field = QLineEdit("")
+        self.description_field.setPlaceholderText("Enter player name or segmentation description here")
+        self.layout().addWidget(self.description_field)
 
         btn = QPushButton("Save to highscore table")
         btn.clicked.connect(self._save_clicked)
@@ -49,7 +69,7 @@ class TheSegmentationGameWidget(QWidget):
             item = self.layout().itemAt(i).widget()
             if item.layout() is not None:
                 item.layout().setSpacing(0)
-                item.layout().setContentsMargins(3, 3, 3, 3)
+                item.layout().setContentsMargins(1, 1, 1, 1)
 
         # ----------------------------------------------------------
         # Thread worker for updating memory consumption threading
@@ -58,14 +78,14 @@ class TheSegmentationGameWidget(QWidget):
         @thread_worker
         def loop_run():
             while self._viewer.window.qt_viewer:  # loop until napari closes
-                yield self.determine_quality()
+                yield self.determine_quality(self.comparison_method_select.value.value)
                 time.sleep(0.5)
 
         # Start the loop
         worker = loop_run()
         worker.yielded.connect(self._update_quality)
         if _for_testing:
-            self._update_quality(self.determine_quality())
+            self._update_quality(self.determine_quality(self.comparison_method_select.value.value))
         else:
             worker.start()
 
@@ -88,18 +108,49 @@ class TheSegmentationGameWidget(QWidget):
             warnings.warn("No ground truth annotation and segmentation result must not be the same!")
             return None
 
+        features = self.ground_truth_annotation_select.value.features
+        if features is None:
+            features = {}
+
+        if not isinstance(features, dict):
+            features = features.to_dict(orient='list')
+
+        # store description
+        if "description" not in features.keys():
+            features["description"] = []
+        features["description"].append(self.description_field.text())
+
+        # store metrics
+        from .metrics import Metrics
+        for metric in Metrics:
+            function = metric.value
+            name = str(metric)
+
+            quality = self.determine_quality(function)
+
+            if name not in features.keys():
+                features[name] = []
+            features[name].append(quality)
+
+        self.ground_truth_annotation_select.value.features = features
+
+        from napari_skimage_regionprops import add_table
+        add_table(self.ground_truth_annotation_select.value, self._viewer)
+
     def _update_quality(self, quality:float):
         """Receive a quality measurement and store it in the user interface"""
-        self.quality_text.setText(str(quality)[0:5])
         if quality is not None:
+            self.quality_text.setText(str(quality)[0:5])
             if quality > 0.9:
                 self.quality_text.setStyleSheet("QLabel { color : green }")
             elif quality > 0.5:
                 self.quality_text.setStyleSheet("QLabel { color : yellow }")
             else:
                 self.quality_text.setStyleSheet("QLabel { color : red }")
+        else:
+            self.quality_text.setText("")
 
-    def determine_quality(self):
+    def determine_quality(self, function):
         """Determine quality of the currently selected label images considering the currently selected metric."""
         if self.ground_truth_annotation_select.value is None:
             return None
@@ -107,9 +158,6 @@ class TheSegmentationGameWidget(QWidget):
             return None
         if self.segmentation_result_select.value is self.ground_truth_annotation_select.value:
             return None
-
-        from .metrics import jaccard_index_sparse
-        function = jaccard_index_sparse
 
         quality = function(self.ground_truth_annotation_select.value.data, self.segmentation_result_select.value.data)
 
