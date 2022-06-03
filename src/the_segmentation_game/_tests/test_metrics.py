5f1838b4bc00cbcdeab7a31885334e86e6a9e73b
Martin Schätz <61615369+martinschatz-cz@users.noreply.github.com>
2022-06-03 19:30:23 +0200

    Added tests for ROC
    For binary accuracy, balanced accuracy, and F1 score.

--- src/the_segmentation_game/_tests/test_metrics.py
+++ src/the_segmentation_game/_tests/test_metrics.py
@@ -48,3 +48,22 @@ def test_jaccard_index_sparse():
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ])
     assert met.jaccard_index_sparse(reference_labels, test_labels) == 0.5
+    
+def test_ROC_binary():
+    reference_labels = np.asarray([
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ])
+    
+    test_labels = np.asarray([
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ])
+    
+    assert met.roc_accuracy_binary(reference_labels, test_labels) == 0.9
+    assert met.roc_balanced_accuracy_binary(reference_labels, test_labels) == 0.75
+    assert met.ROC_F1Score_binary(reference_labels, test_labels) == 2/3
